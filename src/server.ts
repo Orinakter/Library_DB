12952f20439b388755646351269f273d7b614f14
Orinakter <orin.akter9013@gmail.com>
2025-06-20 20:26:00 +0600

    setup express,typesript,mongooes

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import { Server } from "http";
+import app from "./app";
+import mongoose from "mongoose";
+const port = 5000;
+let server: Server;
+
+const connectServer = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://librarayDbUser:piyfRydzhlGYsLUQ@cluster0.fo90p.mongodb.net/library?retryWrites=true&w=majority&appName=Cluster0`
+    );
+    console.log("successfully connected mongoDb atlas");
+    server = app.listen(port, () => {
+      console.log(`library server running on : ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+connectServer();
