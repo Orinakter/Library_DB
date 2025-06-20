4bfd2005a444c5466356b36b21be54b4f63bd9b7
Orinakter <orin.akter9013@gmail.com>
2025-06-20 20:36:59 +0600

    create interfaces,book interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express, { Application, Request, Response } from 'express'
 const app:Application = express();
 
+app.use(express.json())
+
 app.get('/', (req:Request, res:Response) => {
   res.send('Library Db running')
 })
