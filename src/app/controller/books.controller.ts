3ceacac7745b483f7269cd955972df25611aa689
Orinakter <orin.akter9013@gmail.com>
2025-06-20 21:39:22 +0600

    get book by id api route created

--- src/app/controller/books.controller.ts
+++ src/app/controller/books.controller.ts
@@ -3,16 +3,16 @@ import { Book } from "../models/books.modeles";
 
 export const bookRouter = express.Router();
 
+// post book
+
 bookRouter.post("/", async (req: Request, res: Response) => {
   try {
     const book = await Book.create(req.body);
-    res
-      .status(201)
-      .json({
-        success: true,
-        message: "Book created successfully",
-        data: book,
-      });
+    res.status(201).json({
+      success: true,
+      message: "Book created successfully",
+      data: book,
+    });
   } catch (error) {
     res
       .status(400)
@@ -20,16 +20,46 @@ bookRouter.post("/", async (req: Request, res: Response) => {
   }
 });
 
-bookRouter.get('/',async(req: Request, res: Response)=>{
-    try {
-    const { filter, sortBy = 'createdAt', sort = 'desc', limit = '10' } = req.query;
+// get all book
+
+bookRouter.get("/", async (req: Request, res: Response) => {
+  try {
+    const {
+      filter,
+      sortBy = "createdAt",
+      sort = "desc",
+      limit = "10",
+    } = req.query;
     const query = filter ? { genre: filter } : {};
     const books = await Book.find(query)
-      .sort({ [sortBy as string]: sort === 'asc' ? 1 : -1 })
+      .sort({ [sortBy as string]: sort === "asc" ? 1 : -1 })
       .limit(parseInt(limit as string));
 
-    res.json({ success: true, message: 'Books retrieved successfully', data: books });
+    res.json({
+      success: true,
+      message: "Books retrieved successfully",
+      data: books,
+    });
   } catch (error) {
-    res.status(500).json({ success: false, message: 'Failed to get books', error });
+    res
+      .status(500)
+      .json({ success: false, message: "Failed to get books", error });
   }
-})
+});
+
+// get single book by id
+
+bookRouter.get("/:bookId", async (req: Request, res: Response) => {
+  try {
+    const book = await Book.findById(req.params.bookId);
+     res.json({
+      success: true,
+      message: "Books retrieved successfully",
+      data: book,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ success: false, message: "Failed to get book", error });
+  }
+});
