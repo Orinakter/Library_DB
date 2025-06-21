4343523b6bf037d22560e4837aebbc9bc13d9370
Orinakter <orin.akter9013@gmail.com>
2025-06-21 20:32:14 +0600

    get borrow summery created with create aggregation pipeline

--- src/app/controller/borrow.controller.ts
+++ src/app/controller/borrow.controller.ts
@@ -17,14 +17,56 @@ borrowRouter.post("/", async (req: Request, res: Response) => {
       },
       { new: true }
     );
-    if(findBook){
-        await findBook.checkedAvailability()
+    if (findBook) {
+      await findBook.checkedAvailability();
     }
 
     const data = await Borrow.create(body);
     res.status(201).json({
       success: true,
-      message: "Borrow created successfully",
+      message: "Book borrowed successfully",
+      data: data,
+    });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ success: false, message: "Validation failed", error });
+  }
+});
+
+borrowRouter.get("/", async (req: Request, res: Response) => {
+  try {
+    const data = await Borrow.aggregate([
+      {
+        $group: {
+          _id: "$book",
+          totalQuantity: { $sum: "$quantity" },
+        },
+      },
+      {
+        $lookup: {
+          from: "books",
+          localField: "_id",
+          foreignField: "_id",
+          as: "book",
+        },
+      },
+      { $unwind: "$book" },
+      {
+        $project: {
+          _id: 0,
+          book: {
+            title: "$book.title",
+            isbn: "$book.isbn",
+          },
+          totalQuantity: 1,
+        },
+      },
+    ]);
+
+    res.status(201).json({
+      success: true,
+      message: "Borrowed books summary retrieved successfully",
       data: data,
     });
   } catch (error) {
